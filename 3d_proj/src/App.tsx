f530f840f09b618578eb6192f9e1a0f538993738
chaitanya <110461756+chaitanya5420@users.noreply.github.com>
2025-04-19 22:38:39 +0530

    deploy_added

--- 3d_proj/src/App.tsx
+++ 3d_proj/src/App.tsx
@@ -1,32 +1,11 @@
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
-import { useLoader } from '@react-three/fiber';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 
 function DonutModel() {
-  const obj = useLoader(OBJLoader, 'GROUND_FLOOR_LAYOUT.glb');
-
-  return <primitive object={obj} scale={1} />;
-}// import.meta.env.BASE_URL + 
-// function DonutModel() {
-//   const loader = new OBJLoader();
-//   loader.load(
-//     '3d_proj/public/campus.obj',
-//     function (object) {
-//       scene.add(object);
-//     },
-//     function (xhr) {
-//       console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
-//     },
-//     function (error) {
-//       console.error('An error happened', error);
-//     }
-//   );
-  
-//   const { scene } = useGLTF('campus.obj');
-//   return <primitive object={scene} scale={1} />
-// }
+  const { scene } = useGLTF(import.meta.env.BASE_URL + 'GROUND_FLOOR_LAYOUT.glb');
+  return <primitive object={scene} scale={1} />
+}
 
 export default function DonutViewer() {
   return (
